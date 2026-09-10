3c84e33ce5e95b3a231b6c898d6a9f4b3b294fd0
Amanda <133689689+amaqlee@users.noreply.github.com>
2026-09-10 02:08:17 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() % 2 == 1){
+            return false;
+        }
+
+        Stack<Character> half = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char curr = s.charAt(i);
+            if(curr == '(' || curr == '{' || curr == '['){
+                half.push(curr);
+            }else{
+                //curr is a closed bracket
+                //removed should be an open one
+                char removed = half.pop();
+                if((curr == ')' && removed != '(')
+                    || (curr == ']' && removed != '[')
+                    || (curr == '}' && removed != '{')){
+                        return false;
+                }
+            }
+        }
+        if(half.size() != 0){
+            return false;
+        }
+        return true;
+    }
+}
